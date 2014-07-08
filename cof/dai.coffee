56d2d0d41fdd82828bcb9e7c2b9c3b1abc61f8e2
kevin olson <acidjazz@gmail.com>
2014-07-07 22:24:17 -0700

    works for now

--- cof/dai.coffee
+++ cof/dai.coffee
@@ -3,17 +3,68 @@ dai =
   width: 900
   height: 550
   player: false
+  ticker: false
+  outer: false
+  colors: ['black','blue','white','red','grey','turq','mist','green','straw', 'yellow', 'greenblack']
+  slots:
+    '0-5': 'white'
+    '5-18': 'black'
+    '18-23': 'red'
+    '23-29': 'black'
+    '29-34': 'straw'
+    '34-37': 'turq'
+    '37-39': 'mist'
+    '39-49': 'black'
+    '49-54': 'green'
+    '54-64': 'black'
+    '64-73': 'white'
+    '73-80': 'straw'
+    '80-84': 'black'
+    '84-86': 'green'
+    '86-91': 'black'
+    '91-92': 'red'
+    '93-96': 'black'
+    '96-97': 'yellow'
+    '97-98': 'black'
+    '98-99': 'green'
+
+    '99-104': 'black'
+    '104-106': 'blue'
+    '106-118.5': 'black'
+    '118.5-120': 'greenblack'
+    '122-130': 'black'
 
   i: ->
     console.log 'initiated'
     dai.ytApi()
+    dai.ticker = setInterval dai.update, 250
+    dai.outer = $ 'body'
 
+      
   on:
     ready: (event) ->
       console.log 'onReady'
+      dai.player.playVideo()
 
     statechange: (event) ->
       console.log 'onStateChange'
+
+  update: ->
+    return true if !dai.player or typeof dai.player.getCurrentTime isnt 'function'
+    time = dai.player.getCurrentTime()
+    console.log time
+
+    for range, color of dai.slots
+      ranges = range.split('-')
+      if time > parseFloat(ranges[0]) and time < parseFloat(ranges[1])
+        console.log ranges
+        dai.color color
+        return true
+
+  color: (color) ->
+    dai.outer.removeClass col for col in dai.colors
+    dai.outer.addClass color
+
 
   ytApi: ->
     tag = document.createElement 'script'
@@ -23,6 +74,7 @@ dai =
     
     first = document.getElementsByTagName('script')[0]
     first.parentNode.insertBefore tag, first
+
 
   ready: (event) ->
 
--- jst/dai.js
+++ jst/dai.js
@@ -5,17 +5,77 @@ dai = {
   width: 900,
   height: 550,
   player: false,
+  ticker: false,
+  outer: false,
+  colors: ['black', 'blue', 'white', 'red', 'grey', 'turq', 'mist', 'green', 'straw', 'yellow', 'greenblack'],
+  slots: {
+    '0-5': 'white',
+    '5-18': 'black',
+    '18-23': 'red',
+    '23-29': 'black',
+    '29-34': 'straw',
+    '34-37': 'turq',
+    '37-39': 'mist',
+    '39-49': 'black',
+    '49-54': 'green',
+    '54-64': 'black',
+    '64-73': 'white',
+    '73-80': 'straw',
+    '80-84': 'black',
+    '84-86': 'green',
+    '86-91': 'black',
+    '91-92': 'red',
+    '93-96': 'black',
+    '96-97': 'yellow',
+    '97-98': 'black',
+    '98-99': 'green',
+    '99-104': 'black',
+    '104-106': 'blue',
+    '106-118.5': 'black',
+    '118.5-120': 'greenblack',
+    '122-130': 'black'
+  },
   i: function() {
     console.log('initiated');
-    return dai.ytApi();
+    dai.ytApi();
+    dai.ticker = setInterval(dai.update, 250);
+    return dai.outer = $('body');
   },
   on: {
     ready: function(event) {
-      return console.log('onReady');
+      console.log('onReady');
+      return dai.player.playVideo();
     },
     statechange: function(event) {
       return console.log('onStateChange');
     }
+  },
+  update: function() {
+    var color, range, ranges, time, _ref;
+    if (!dai.player || typeof dai.player.getCurrentTime !== 'function') {
+      return true;
+    }
+    time = dai.player.getCurrentTime();
+    console.log(time);
+    _ref = dai.slots;
+    for (range in _ref) {
+      color = _ref[range];
+      ranges = range.split('-');
+      if (time > parseFloat(ranges[0]) && time < parseFloat(ranges[1])) {
+        console.log(ranges);
+        dai.color(color);
+        return true;
+      }
+    }
+  },
+  color: function(color) {
+    var col, _i, _len, _ref;
+    _ref = dai.colors;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      col = _ref[_i];
+      dai.outer.removeClass(col);
+    }
+    return dai.outer.addClass(color);
   },
   ytApi: function() {
     var first, tag;
